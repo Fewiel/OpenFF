3374f73c2a95ec42686aafb903682da62a8f2e18
Phillip Weitkamp <pweitkamp@outlook.de>
2022-04-10 17:48:43 +0200

    Add Rank;

---
+++ OpenFF.Adapter.LiteDB/Rank/Rank.cs
@@ -0,0 +1,23 @@
+﻿using LiteDB;
+using OpenFF.DataAccessLayer.Rank;
+
+namespace OpenFF.Adapter.LiteDB;
+
+public class Rank : IRank
+{
+    public long ID { get; set; }
+    public string Name { get; set; }
+    public string Picture { get; set; }
+
+    [BsonCtor]
+#pragma warning disable CS8618 // Ein Non-Nullable-Feld muss beim Beenden des Konstruktors einen Wert ungleich NULL enthalten. Erwägen Sie die Deklaration als Nullable.
+    private Rank() { }
+#pragma warning restore CS8618 // Ein Non-Nullable-Feld muss beim Beenden des Konstruktors einen Wert ungleich NULL enthalten. Erwägen Sie die Deklaration als Nullable.
+
+    public Rank(long iD, string name, string picture)
+    {
+        ID = iD;
+        Name = name;
+        Picture = picture;
+    }
+}
---
+++ OpenFF.Adapter.LiteDB/Rank/RankRepository.cs
@@ -0,0 +1,68 @@
+﻿using LiteDB;
+using OneOf;
+using OneOf.Types;
+using OpenFF.DataAccessLayer.Exceptions;
+using OpenFF.DataAccessLayer.Rank;
+
+namespace OpenFF.Adapter.LiteDB;
+
+public class RankRepository : IRankRepository
+{
+    private readonly ILiteCollection<Rank> Col;
+
+    public RankRepository(ILiteCollection<Rank> col)
+    {
+        Col = col;
+        Col.EnsureIndex(x => x.ID);
+    }
+
+    public void Create(IRank data)
+    {
+        if (data is not Rank r)
+            throw new UnsupportedDatabaseException();
+
+        Col.Insert(r);
+    }
+
+    public void Delete(IRank data) => Col.Delete(data.ID);
+
+    public void Delete(long id) => Col.Delete(id);
+
+    public OneOf<IRank, None> Get(long id)
+    {
+        var r = Col.FindOne(x => x.ID == id);
+        if (r == null)
+            return new None();
+
+        return r;
+    }
+
+    public IEnumerable<IRank> GetAll() => Col.FindAll();
+
+    public long GetCount() => Col.LongCount();
+
+    public long GetMaxID() => Col.Max();
+
+    public long GetMinID() => Col.Min();
+
+    public IEnumerable<IRank> GetRange(long offset, int amount, string orderField, bool orderAsc)
+    {
+        if (orderField == "ID")
+            orderField = "_id";
+
+        return Col.Query().OrderBy(orderField, orderAsc ? 1 : 0).Skip((int)offset).Limit(amount).ToEnumerable();
+    }
+
+    public IRank New(long id, string name, string picture)
+    {
+        return new Rank(id, name, picture);
+    }
+
+    public void Update(IRank data)
+    {
+        if (data is not User r)
+            throw new UnsupportedDatabaseException();
+
+        Col.Update(r);
+    }
+}
