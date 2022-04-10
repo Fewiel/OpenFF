fab86db2c879e262c1950999ccb562a5392f9c5b
Phillip Weitkamp <pweitkamp@outlook.de>
2022-04-10 17:20:47 +0200

    Add Rank

---
+++ OpenFF.DataAccessLayer/Rank/IRank.cs
@@ -0,0 +1,8 @@
+﻿namespace OpenFF.DataAccessLayer.Rank;
+
+public interface IRank
+{
+    long ID { get; }
+    string Name { get; }
+    string Picture { get; }
+}
---
+++ OpenFF.DataAccessLayer/Rank/IRankRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace OpenFF.DataAccessLayer.Rank;
+
+public interface IRankRepository : IRepositoryBase<IRank>
+{
+    IRank New(long id, string name, string picture);
+}
